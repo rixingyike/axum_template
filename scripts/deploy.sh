29318b4887995b183abe7b70994a62143e4e9743
李艺 <9830131@qq.com>
2025-03-19 14:01:57 +0800

    自动提交: 2025-03-19_14:01:56

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+PROJECT_NAME=$(basename "$(pwd)")
+
 # 服务器配置
-SERVER="user@example.com"
-APP_DIR="/var/www/axum_app"
+SERVER="Administrator@81.70.152.107"
+APP_DIR="c:/work/$PROJECT_NAME"
 SERVICE_NAME="axum_service"
 
 # 代码同步
-rsync -avz --delete --exclude='.git' --exclude='target' ./ ${SERVER}:${APP_DIR}
+rsync -avz --delete --exclude='.git' --exclude='target' --rsh='ssh' ./ ${SERVER}:${APP_DIR} 
 
 # 远程构建与重启
 ssh ${SERVER} << EOF
   cd ${APP_DIR}
   cargo build --release
-  sudo systemctl restart ${SERVICE_NAME}
+#   sudo systemctl restart ${SERVICE_NAME}
 EOF
 
 echo "Deployment completed!"
